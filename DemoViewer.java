7df615a62044cdc66345b40a7f933d7b16868e63
Vikash Kushwaha <124918730+Vikash3456@users.noreply.github.com>
2024-07-19 00:08:09 +0530

    GUI[WRAPPER]

---
+++ DemoViewer.java
@@ -0,0 +1,34 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class DemoViewer {
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        Container pane = frame.getContentPane();
+        pane.setLayout(new BorderLayout());
+
+        // slider to control horizontal rotation
+        JSlider headingSlider = new JSlider(0, 360, 180);
+        pane.add(headingSlider, BorderLayout.SOUTH);
+
+        // slider to control vertical rotation
+        JSlider pitchSlider = new JSlider(SwingConstants.VERTICAL, -90, 90, 0);
+        pane.add(pitchSlider, BorderLayout.EAST);
+
+        // panel to display render results
+        JPanel renderPanel = new JPanel() {
+                public void paintComponent(Graphics g) {
+                    Graphics2D g2 = (Graphics2D) g;
+                    g2.setColor(Color.BLACK);
+                    g2.fillRect(0, 0, getWidth(), getHeight());
+
+                    // rendering magic will happen here
+                }
+            };
+        pane.add(renderPanel, BorderLayout.CENTER);
+
+        frame.setSize(400, 400);
+        frame.setVisible(true);
+    }
+}
